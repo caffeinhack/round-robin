1a692cd173dcdf95214324fae648f2e9a900f3ab
akakou <akakou571@gmail.com>
2017-08-10 23:20:20 +0900

    [update] init method setting character list

--- character_list.py
+++ character_list.py
@@ -13,13 +13,52 @@ import string
 class CharacterList:
     '''This class is stationery for list of characters.'''
 
-    def __init__(self, any_characters=[], alphabets=False, numbers=False):
+    def __init__(
+            self,
+            any_characters=[],
+            use_alphabets=False,
+            use_large_alphabets=False,
+            use_small_alphabets=False,
+            use_numbers=False,
+            use_all=False
+            ):
         '''Set up which characters using for making strings.'''
-        self.character_list = any_characters
+        self.reset()
+        self.add_any(any_characters)
 
-    def add_any(self, any_characters=[]):
+        # Check using all and
+        # add to character list.
+        if use_all:
+            self.add_alphabets()
+            self.add_numbers()
+            return
+
+        # Check using alphabets and
+        # add to character_list.
+        if use_alphabets:
+            self.add_alphabets()
+        else:
+            # Check using small and large alphabets so.
+            if use_small_alphabets:
+                self.add_small_alphabets()
+            elif use_large_alphabets:
+                self.add_large_alphabets()
+
+        # Check using numbers and
+        # add to character_list.
+        if use_numbers:
+            self.add_numbers()
+
+    def reset(self):
+        '''Reset character_list.'''
+        self.character_list = []
+
+    def add_any(self, any_characters):
         '''Add any list of character.'''
-        self.character_list.append(any_characters)
+        # check variable type
+        if not isinstance(any_characters, list):
+            any_characters = list(any_characters)
+        self.character_list.extend(any_characters)
 
     def add_alphabets(self):
         '''Add alphabets.'''
@@ -27,21 +66,21 @@ class CharacterList:
         self.add_any(all_alphabets)
 
     def add_small_alphabets(self):
-        '''Add small aplphabets.'''
+        '''Add small alphabets.'''
         all_alphabets = string.ascii_letters
         self.add_any(all_alphabets[0:26])
 
     def add_large_alphabets(self):
-        '''Add large aplphabets.'''
+        '''Add large alphabets.'''
         all_alphabets = string.ascii_letters
         self.add_any(all_alphabets[26:])
 
-    def add_integers(self):
-        '''Add integers.'''
-        # make character list of integers
-        integers = []
-        for count in range(0, 9):
+    def add_numbers(self):
+        '''Add numbers.'''
+        # make character list of numbers
+        numbers = []
+        for count in range(0, 10):
             count = str(count)
-            integers.append(integers)
+            numbers.append(count)
 
-        self.add_any(integers)
+        self.add_any(numbers)
